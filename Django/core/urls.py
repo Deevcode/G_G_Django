f4951dba816879661a7c748c2bd0de9a1ed1d3a8
Jordaan23 <jo.cornejoz@duocuc.cl>
2022-05-20 20:01:57 -0400

    cambios en contacto.html

--- Django/core/urls.py
+++ Django/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import galeria, home 
+from .views import galeria, home , contacto
 
 urlpatterns = [
     path('', home,name='home'),
-    path('galeria/', galeria,name='galeria'),   
+    path('galeria/', galeria,name='galeria'),
+    path('contacto/', contacto,name='contacto'),   
 ]
--- Django/core/views.py
+++ Django/core/views.py
@@ -2,8 +2,16 @@ from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
+
+# VISTA DE HOME HTML #
 def home (request):
     return render(request, 'core/home.html')
 
+# VISTA DE GALERIA HTML #
 def galeria(request):
-    return render(request, 'core/galeria.html')    
+    return render(request, 'core/galeria.html') 
+
+# VISTA DE CONTACTO HTML #
+
+def contacto(request):
+    return render(request, 'core/contacto.html')
