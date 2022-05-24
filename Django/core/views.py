a4f7df271584dd512ebfb49bea115f611cab5bfa
Jordaan23 <josee.cornejo@gmail.com>
2022-05-24 01:22:43 -0400

    Modificacion en la base de datos, modelos y vistas

--- Django/core/views.py
+++ Django/core/views.py
@@ -1,11 +1,17 @@
+from operator import imod
 from unicodedata import name
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 # VISTA DE HOME HTML #
 def home (request):
-    return render(request, 'core/home.html')
+    productos = Producto.objects.all()
+    data = {
+        'productos' : productos
+    }
+    return render(request, 'core/home.html', data)
 
 # VISTA DE GALERIA HTML #
 def galeria(request):
