a8636dcf955ed8b7e691a9d42626973b8ad5fb08
Jordaan23 <josee.cornejo@gmail.com>
2022-06-03 00:58:34 -0400

    subiendo formularios e integrando bootstrap

--- Django/core/urls.py
+++ Django/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import galeria, home , contacto
+from .views import galeria, home , contacto , agregar_producto
 
 urlpatterns = [
-    path('', home,name='home'),
-    path('galeria/', galeria,name='galeria'),
-    path('contacto/', contacto,name='contacto'),   
+    path('', home,name="home"),
+    path('galeria/', galeria,name="galeria"),
+    path('contacto/', contacto,name="contacto"),
+    path('agregar-producto/', agregar_producto,name="agregar_producto"),   
 ]
