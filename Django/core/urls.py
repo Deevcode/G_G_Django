98bc82f41dd786b9893adfaa027f081a4f40421c
Jordaan23 <josee.cornejo@gmail.com>
2022-06-04 22:53:05 -0400

    cambios importantes, hmtl agregados fundacion, cambios en html listar agregar

--- Django/core/urls.py
+++ Django/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import galeria, home , contacto , agregar_producto
+from .views import fundacion_page, galeria, home , contacto , agregar_producto, listar_producto
 
 urlpatterns = [
     path('', home,name="home"),
     path('galeria/', galeria,name="galeria"),
     path('contacto/', contacto,name="contacto"),
-    path('agregar-producto/', agregar_producto,name="agregar_producto"),   
+    path('agregar-producto/', agregar_producto,name="agregar_producto"),
+    path('listar-producto/', listar_producto, name="listar_producto"),
+    path('fundacion/', fundacion_page, name="fundacion"),
 ]
