1a210a77d790f81faebe3e614e64ad5d8f9bbb5a
Moises <moiserubio2014@gmail.com>
2022-06-05 17:28:04 -0400

    borrando y actulaizando repositorio

--- Django/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import galeria, home , contacto
-
-urlpatterns = [
-    path('', home,name='home'),
-    path('galeria/', galeria,name='galeria'),
-    path('contacto/', contacto,name='contacto'),   
-]
