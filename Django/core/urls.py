f4dd907a316c6317bb9f0a5664d8124e9e223ef2
Jordaan23 <josee.cornejo@gmail.com>
2022-06-05 23:34:01 -0400

    cambios por problemas de merge+

---
+++ Django/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import fundacion_page, galeria, home , contacto , agregar_producto, listar_producto, modificar_producto
+
+urlpatterns = [
+    path('', home,name="home"),
+    path('galeria/', galeria,name="galeria"),
+    path('contacto/', contacto,name="contacto"),
+    path('agregar-producto/', agregar_producto,name="agregar_producto"),
+    path('listar-producto/', listar_producto, name="listar_producto"),
+    path('fundacion/', fundacion_page, name="fundacion"),
+    path('modificar-producto/<id>/', modificar_producto, name="modificar_producto"),
+]
